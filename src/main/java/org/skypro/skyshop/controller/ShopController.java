a29e662a4b1b359efd81f6d134566041c4beeb20
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-05-31 19:55:14 +0400

    initial commit

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -4,7 +4,6 @@ import org.skypro.skyshop.model.article.Article;
 import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
-
 import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.SearchService;
 import org.skypro.skyshop.service.StorageService;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -45,7 +45,7 @@ public class ShopController {
     }
 
     @GetMapping("/basket/{id}")
-    public String addProduct(@PathVariable("id")UUID id) {
+    public String addProduct(@PathVariable("id") UUID id) {
         basketService.addProduct(id);
         return "продукт добавлен в корзину";
     }
@@ -54,32 +54,4 @@ public class ShopController {
     public UserBasket getUserBasket() {
         return basketService.getUserBasket();
     }
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
