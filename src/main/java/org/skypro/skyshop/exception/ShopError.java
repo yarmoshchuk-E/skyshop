9f3c427101b3e5072af800f1d1fb9faec183df02
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-05-31 19:56:45 +0400

    выполнил д/з

--- src/main/java/org/skypro/skyshop/exception/ShopError.java
+++ src/main/java/org/skypro/skyshop/exception/ShopError.java
@@ -1,12 +1,12 @@
 package org.skypro.skyshop.exception;
-//
-//public record ShopError(String code, String message) {
-//
-//    public String getCode() {
-//        return code;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//}
+
+public record ShopError(String code, String message) {
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
